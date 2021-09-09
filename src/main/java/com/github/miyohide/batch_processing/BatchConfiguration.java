ae1dd14a4c34cf03aa9efdeb9e066cb1195c4dea
Hidenori Miyoshi <miyohide@gmail.com>
2021-09-09 20:49:54 +0900

    delete unused import statements

--- src/main/java/com/github/miyohide/batch_processing/BatchConfiguration.java
+++ src/main/java/com/github/miyohide/batch_processing/BatchConfiguration.java
@@ -15,11 +15,9 @@ import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 import javax.sql.DataSource;
-import java.nio.file.Paths;
 
 @Configuration
 @EnableBatchProcessing
