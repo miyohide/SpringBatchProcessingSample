6034907d60bad38f3c5473c454d29ee17675b14c
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-09 19:01:48 +0900

    create tfvars file.

--- deploy/aci_with_acr/01_prepare/main.tf
+++ deploy/aci_with_acr/01_prepare/main.tf
@@ -1,11 +1,21 @@
+variable "app_resource_group_name" {}
+variable "app_resource_group_location" {}
+variable "db_user_key" {}
+variable "db_password_key" {}
+variable "container_registry_name" {}
+variable "postgresql_server_name" {}
+variable "postgresql_db_name" {}
+variable "container_instance_storage_account_name" {}
+variable "container_instance_storage_share_name" {}
+
 provider "azurerm" {
   features {}
 }
 
 # resource groupの作成
 resource "azurerm_resource_group" "rg" {
-  location = "japaneast"
-  name     = "rg-batch001"
+  location = var.app_resource_group_location
+  name     = var.app_resource_group_name
 }
 
 # RDBMSのユーザ名とパスワードの参照のために既存のKeyVaultを参照
@@ -16,18 +26,18 @@ data "azurerm_key_vault" "kv" {
 
 data "azurerm_key_vault_secret" "db-user" {
   key_vault_id = data.azurerm_key_vault.kv.id
-  name         = "app-db-user"
+  name         = var.db_user_key
 }
 
 data "azurerm_key_vault_secret" "db-password" {
   key_vault_id = data.azurerm_key_vault.kv.id
-  name         = "app-db-password"
+  name         = var.db_password_key
 }
 
 # Azure Container Registryの作成
 resource "azurerm_container_registry" "acr" {
   location            = azurerm_resource_group.rg.location
-  name                = "acrmiyohide001"
+  name                = var.container_registry_name
   resource_group_name = azurerm_resource_group.rg.name
   sku = "Basic"
   admin_enabled = true
@@ -36,7 +46,7 @@ resource "azurerm_container_registry" "acr" {
 # PostgreSQLの作成
 resource "azurerm_postgresql_server" "pg" {
   location            = azurerm_resource_group.rg.location
-  name                = "pgmiyohidedb001"
+  name                = var.postgresql_server_name
   resource_group_name = azurerm_resource_group.rg.name
   sku_name            = "B_Gen5_1"
   version             = "11"
@@ -54,7 +64,7 @@ resource "azurerm_postgresql_server" "pg" {
 resource "azurerm_postgresql_database" "pg-db" {
   charset             = "utf8"
   collation           = "Japanese_Japan.932"
-  name                = "app_db_production"
+  name                = var.postgresql_db_name
   resource_group_name = azurerm_resource_group.rg.name
   server_name         = azurerm_postgresql_server.pg.name
 }
@@ -64,13 +74,13 @@ resource "azurerm_storage_account" "sa" {
   account_replication_type = "LRS"
   account_tier             = "Standard"
   location                 = azurerm_resource_group.rg.location
-  name                     = "samiyohidebatch001"
+  name                     = var.container_instance_storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
 }
 
 # File Shareの作成
 resource "azurerm_storage_share" "ss" {
-  name                 = "aci-test-share"
+  name                 = var.container_instance_storage_share_name
   storage_account_name = azurerm_storage_account.sa.name
   quota                = 10
 }
