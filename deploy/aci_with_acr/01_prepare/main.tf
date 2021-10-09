af160061c1ab99491ca0ed68df58e0aad67a7c5c
Hidenori Miyoshi <miyohide@gmail.com>
2021-10-09 21:11:04 +0900

    move variables to file

--- deploy/aci_with_acr/01_prepare/main.tf
+++ deploy/aci_with_acr/01_prepare/main.tf
@@ -1,13 +1,3 @@
-variable "app_resource_group_name" {}
-variable "app_resource_group_location" {}
-variable "db_user_key" {}
-variable "db_password_key" {}
-variable "container_registry_name" {}
-variable "postgresql_server_name" {}
-variable "postgresql_db_name" {}
-variable "container_instance_storage_account_name" {}
-variable "container_instance_storage_share_name" {}
-
 provider "azurerm" {
   features {}
 }
--- deploy/aci_with_acr/01_prepare/variables.tf
+++ deploy/aci_with_acr/01_prepare/variables.tf
@@ -5,3 +5,13 @@ variable "kv_name" {
 variable "kv_rg" {
   type = string
 }
+
+variable "app_resource_group_name" {}
+variable "app_resource_group_location" {}
+variable "db_user_key" {}
+variable "db_password_key" {}
+variable "container_registry_name" {}
+variable "postgresql_server_name" {}
+variable "postgresql_db_name" {}
+variable "container_instance_storage_account_name" {}
+variable "container_instance_storage_share_name" {}
